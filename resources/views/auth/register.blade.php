bcc19bb03816c0b917bbebf186db7a5c35e37582
Marco <marco.v1991@outlook.com>
2018-07-09 13:51:11 -0400

    muestras-registro_login_e_inicio

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,125 @@
 @extends('layouts.app')
 
+@section('body-class','signup-page')
+
 @section('content')
+<div class="header header-filter" style="background-image: url('{{ asset('img/lab_quimico.jpg') }}'); background-size: cover; background-position: top center;">
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<div class="row">
+<div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+    <div class="card card-signup">
+        <form class="form" method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
+                <!--token-->
+                @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
-                        @csrf
+            <div class="header header-primary text-center">
+                <h4>Registro de nuevo Usuario</h4>
+<!--                 <div class="social-line">
+                    <a href="#pablo" class="btn btn-simple btn-just-icon">
+                        <i class="fa fa-facebook-square"></i>
+                    </a>
+                    <a href="#pablo" class="btn btn-simple btn-just-icon">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a href="#pablo" class="btn btn-simple btn-just-icon">
+                        <i class="fa fa-google-plus"></i>
+                    </a>
+                </div> -->
+            </div>
+            <p class="text-divider">Completa tus datos</p>
+            <div class="content">
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+<!-- NOMBRE -->
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">face</i>
+                    </span>
+                    <input type="text" class="form-control" placeholder="Nombre" name="name" value="{{ old('name') }}" required autofocus>
+                </div>
+<!-- EMAIL -->
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">email</i>
+                    </span>
+                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Correo electrónico" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                        @if ($errors->has('email'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                </div>
+<!-- CONTRASEÑA -->
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">lock_outline</i>
+                    </span>
+                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Contraseña" required>
 
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                        @if ($errors->has('password'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                </div>
+<!-- CONFIRMAR CONTRASEÑA -->
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">lock_outline</i>
+                    </span>
+                        <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password_confirmation" placeholder="Confirmar contraseña" required>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                        @if ($errors->has('password'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
                 </div>
             </div>
-        </div>
+
+            <div class="footer text-center">
+                <button type="submit" href="#pablo" class="btn btn-simple btn-primary btn-lg">CONFIRMAR REGISTRO
+            </div>
+
+                <!--<a class="btn btn-link" href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>-->
+        </form>
     </div>
+</div>
+</div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+    <nav class="pull-left">
+        <ul>
+            <li>
+                <a href="http://www.creative-tim.com">
+                    Creative Tim
+                </a>
+            </li>
+            <li>
+                <a href="http://presentation.creative-tim.com">
+                   About Us
+                </a>
+            </li>
+            <li>
+                <a href="http://blog.creative-tim.com">
+                   Blog
+                </a>
+            </li>
+            <li>
+                <a href="http://www.creative-tim.com/license">
+                    Licenses
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <div class="copyright pull-right">
+        &copy; 2016, made with <i class="fa fa-heart heart"></i> by <a href="http://www.creative-tim.com" target="_blank">Creative Tim</a>
+    </div>
+    </div>
+</footer>
 </div>
 @endsection
